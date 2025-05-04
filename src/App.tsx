670742dd69259fdf58760e3d731e91c5eb125eec
KozyaSkyworker <d_koz@inbox.ru>
2025-05-04 07:39:36 +0400

    feat: make example screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import styles from "./App.module.css";
+
+// TODO:
+// иконки (внутри приложения)
+// шрифты
+// скругления
+
 function App() {
   return (
-    <div>
-      <h1>123</h1>
+    <div className={styles.app}>
+      <h1>Экзаменнационное задание</h1>
+      <p>
+        Время в работе: <span>1ч 24мин</span>
+      </p>
+      <div className={styles.app__line}></div>
+      <div>
+        <p>
+          <span>78%</span> - релевантный контент
+        </p>
+        <p>
+          До дедлайна: <span>2</span> дня
+        </p>
+      </div>
+      <div className={styles.app__divider}></div>
+      <div>
+        <h2>Последние сайты:</h2>
+        <ul>
+          <li>first</li>
+          <li>second seconds</li>
+          <li>third third third third third</li>
+          <li>fourth fourth fourth</li>
+        </ul>
+      </div>
     </div>
   );
 }
