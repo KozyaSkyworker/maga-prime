2ef71b96875799fc190d895cbabb70ca7d01b2de
KozyaSkyworker <d_koz@inbox.ru>
2025-05-29 20:36:03 +0400

    feat: start make classification requests

--- src/screens/exercise/Exercise.tsx
+++ src/screens/exercise/Exercise.tsx
@@ -19,17 +19,16 @@ import { Button, Divider, Progress } from "../../shared/ui";
 import { ROUTES } from "../../shared/routes";
 
 import styles from "./Exercise.module.css";
+import { useState } from "react";
 
 const PERCENT = 78;
 
 const Exercise = () => {
   const { id } = useParams();
-
   const navigate = useNavigate();
 
-  const handleRedirectToReport = () => {
-    navigate(`${ROUTES.REPORTS}/${data?.exercise.id}`);
-  };
+  const [isClassifacationPending, setIsClassificationPending] = useState(false);
+  const [text] = useState("text");
 
   const { data, isLoading } = useFetchData<TExercise>({
     url: `${BASE_BACK_URL}/exercises/${id}`,
@@ -48,8 +47,6 @@ const Exercise = () => {
     chrome.runtime.sendMessage(
       { action: "startListening", exerciseId },
       (response: { status: string; exerciseId: number }) => {
-        console.log("Ответ от background:", response);
-
         if (response.status === "started") {
           mutationRequest({ status: TExerciseStatuses.PROCESS }).then(() =>
             window.location.reload(),
@@ -66,33 +63,60 @@ const Exercise = () => {
     chrome.runtime.sendMessage(
       { action: "stopListening", exerciseId },
       (response: { status: string; exerciseId: number }) => {
-        console.log("Ответ от background:", response);
-
         if (response.status === "stopped") {
-          mutationRequest({ status: TExerciseStatuses.FINISHED }).then(() =>
-            window.location.reload(),
-          );
+          handleStopExerciseAndClassificate();
         }
       },
     );
-    // TODO: reload для тригера рефетча?
+  };
 
-    // fetch("http://127.0.0.1:11434/api/chat", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     model: "gemma3:4b",
-    //     messages: [
-    //       {
-    //         role: "user",
-    //         content: `Ответь да или нет. Следующий текст "${textContent}" соотносится с тематикой моего задания "${EXERSICE_NAME}"`,
-    //       },
-    //     ],
-    //     stream: false,
-    //   }),
-    // })
+  async function handleStopExerciseAndClassificate() {
+    setIsClassificationPending(true);
+
+    // TODO: hook на fetch
+    const [first] = await Promise.allSettled([
+      fetch("http://127.0.0.1:11434/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          model: "gemma3:4b",
+          messages: [
+            {
+              role: "user",
+              content: `Ответь в виде массива JSON, где каждый элемент — это ответ "да" или "нет" при сравнении каждой из следующих фраз на соотношение фразы к фразе "Теория вероятности": ${data?.urls.map((itm) => itm.title).join(";")}`,
+            },
+          ],
+          stream: false,
+        }),
+      }),
+      mutationRequest({ status: TExerciseStatuses.FINISHED }),
+    ]);
+
+    // @ts-expect-error TODO: fix
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const d = await first.value.json();
+
+    // TODO: hook на fetch
+    //  TODO: fix
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const res = await fetch(`${BASE_BACK_URL}/urls/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        // TODO:
+        // url: id, итог из пред. запроса ?!??!?!?!?!?
+      }),
+    });
+
+    setIsClassificationPending(false);
+  }
+
+  const handleRedirectToReport = () => {
+    navigate(`${ROUTES.REPORTS}/${data?.exercise.id}`);
   };
 
   if (isLoading) {
@@ -134,11 +158,16 @@ const Exercise = () => {
           <h2>Посещенные сайты:</h2>
           {/* TODO: message if empty */}
           {data.urls.length > 0 ? (
-            <ul>
+            <ul className={styles.Exercise__items}>
               {data.urls.map((itm) => (
-                <li key={itm.id} className={styles.Exercise__site}>
-                  <CheckIcon />
-                  {itm.url}
+                <li key={itm.id} className={styles.Exercise__item}>
+                  <div className={styles.Exercise__svgWrapper}>
+                    <CheckIcon />
+                  </div>
+                  <div className={styles.Exercise__siteData}>
+                    <strong>{itm.url}</strong>
+                    <p>{itm.title}</p>
+                  </div>
                 </li>
               ))}
             </ul>
@@ -163,11 +192,19 @@ const Exercise = () => {
         )}
         {data.exercise.status === TExerciseStatuses.FINISHED && (
           <Button
-            text="К полному отчету"
+            text={isClassifacationPending ? "Обработка..." : "К полному отчету"}
             onClick={handleRedirectToReport}
             variant="info"
+            disabled={isClassifacationPending}
           />
         )}
+        <button
+          onClick={handleStopExerciseAndClassificate}
+          disabled={isClassifacationPending}
+        >
+          click {String(isClassifacationPending)}
+        </button>
+        {text}
       </div>
     </div>
   );
